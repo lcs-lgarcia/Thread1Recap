5e1bfd5e8631e890e62b350fc980b3a85976b613
Lucas Garcia <lgarciaalonso@lcs.on.ca>
2023-01-10 11:11:40 -0500

    Initial Commit

---
+++ Thread1Recap/Thread1RecapApp.swift
@@ -0,0 +1,17 @@
+//
+//  Thread1RecapApp.swift
+//  Thread1Recap
+//
+//  Created by Lucas García on 10/1/23.
+//
+
+import SwiftUI
+
+@main
+struct Thread1RecapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
